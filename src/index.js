e9376f2d17974a289ac17f897a03cf6be726d4ef
chinthaka <chinthakarukshan@gmail.com>
2022-05-15 00:29:09 +0530

    Added the capability to switch the player

--- src/index.js
+++ src/index.js
@@ -27,13 +27,17 @@ import './index.css';
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -44,7 +48,8 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
+      const player = this.state.xIsNext ? 'X' : 'O';
+      const status = 'Next player: ' + player;
   
       return (
         <div>
